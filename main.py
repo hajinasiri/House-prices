11ad0126702094987c4405768b93d092d09824d4
Shahab <hajinasiri@gmail.com>
2021-01-31 14:00:25 -0800

    splitting train data to train and test

--- main.py
+++ main.py
@@ -4,9 +4,6 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 test_data = pd.read_csv("test.csv")
 train_data = pd.read_csv("train.csv")
-df = train_data
-bf = train_data
-df = df.fillna(df.mean())
 #create a dictionay of most frequent value in each column
 def clean(data):
     #This function takes a dataframe as its variable and cleans it
@@ -21,9 +18,20 @@ train_data = clean(train_data)
 test_data = clean(test_data)
 ctrain = train_data.count()
 ctest = test_data.count()
+#Converting non-numerical columns to numerical
+st_columns = train_data.select_dtypes(include= 'object')
+st_columns = st_columns.columns
+from sklearn.preprocessing import LabelEncoder
+enc = LabelEncoder()
+for column in st_columns:
+    enc.fit(train_data[column])
+    train_data[column] = enc.transform(train_data[column])
+    test_data[column] = enc.transform(test_data[column])
+
 # sns.heatmap(train_data.isnull(),yticklabels=False,cbar=False,cmap='viridis')
 # plt.show()
-
-
-
-
+# splitting train data intor training and testing set
+y = train_data["SalePrice"]
+X = train_data.drop('SalePrice',axis=1)
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
